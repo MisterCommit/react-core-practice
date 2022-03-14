defb1e0b8aca4d9dca0dd7eb75c5d015507bdca2
ahmed <ahmedsiddiqui551@gmail.com>
2022-03-15 00:18:01 +0500

    Update equationGen.js

--- src/AlgoPractics/equationGen.js
+++ src/AlgoPractics/equationGen.js
@@ -1,3 +1,36 @@
+const targetNumber = 21;
+
+let possibleWay = [];
+
+const validate = (firstNumber, secondNumber, firstEquation, secondEquation) => {
+  if (firstNumber + secondNumber === targetNumber) {
+    possibleWay.push(`${firstEquation} + ${secondEquation}`);
+  }
+  if (firstNumber * secondNumber === targetNumber) {
+    possibleWay.push(`${firstEquation} x ${secondEquation}`);
+  }
+  if (firstNumber - secondNumber === targetNumber) {
+    possibleWay.push(`${firstEquation} - ${secondEquation}`);
+  }
+  if (secondNumber - firstNumber === targetNumber) {
+    possibleWay.push(`${firstEquation} - ${secondEquation}`);
+  }
+};
+
+const returnNumber = (number) => {
+  if (number.res) {
+    return number.res;
+  }
+  return number;
+};
+
+const retunEquation = (equation) => {
+  if (equation.str) {
+    return `( ${equation.str} )`;
+  }
+  return equation.toString();
+};
+
 const equ = (array) => {
   let left = 0;
   let right = array.length - 1;
@@ -6,8 +39,35 @@ const equ = (array) => {
 };
 
 const _equ = (array, left, right) => {
-  const element = array[left];
-  console.log(element);
-  let rightEqu = _equ(array, left + 1, right);
-  let leftEqu = _equ(array, 0, left - 1);
+  const results = [];
+  if (left > right) {
+    return results;
+  }
+  if (left === right) {
+    results.push(array[left]);
+    return results;
+  }
+
+  for (let i = left; i < right; i++) {
+    let result1 = _equ(array, left, i);
+    let result2 = _equ(array, i + 1, right);
+    for (let x = 0; x <= result1.length - 1; x++) {
+      for (let y = 0; y <= result2.length - 1; y++) {
+        let firstNumber = returnNumber(result1[x]);
+        let secondNumber = returnNumber(result2[y]);
+        let firstEquation = retunEquation(result1[x]);
+        let secondEquation = retunEquation(result2[y]);
+
+        validate(firstNumber, secondNumber, firstEquation, secondEquation);
+
+        results.push({
+          res: firstNumber + secondNumber,
+          str: `${firstEquation} + ${secondEquation}`,
+        });
+      }
+    }
+  }
+  return results;
 };
+
+equ([2, 3, 7, 24]);
