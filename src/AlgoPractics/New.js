e8d56a1b231f74049892cab2b3644cea96fb9fb7
AhmedRazaVerge <64129958+AhmedRazaVerge@users.noreply.github.com>
2022-03-15 20:24:43 +0500

    Update New.js

--- src/AlgoPractics/New.js
+++ src/AlgoPractics/New.js
@@ -5,21 +5,28 @@ const equ = (array) => {
   return result;
 };
 const _equ = (array, left, right) => {
-  if (right === left) {
-    return [];
-  } else if (right < left) {
+  if (right < left) {
     return [];
   }
-  //   console.log(array, left, right);
   const backArrayOfelements = array.slice(0, left);
   const nextArrayOfelements = array.slice(left + 1, right + 1);
 
+  console.log("===>>>", backArrayOfelements, array[left], nextArrayOfelements);
+  //   console.log(array, left, right);
+
   const leftEquations = _equ(array, 0, left - 1);
   const rightEquations = _equ(array, left + 1, right);
+  const reults = [];
+  for (let i = 0; i < rightEquations.length; i++) {
+    reults.push(`${backArrayOfelements} ${array[left]} ${rightEquations[i]}`);
+  }
 
-  console.log(backArrayOfelements, array[left], nextArrayOfelements);
+  console.log("leftEquations", leftEquations, rightEquations);
+  return reults;
 };
 
 // equ(["a", "b"]);
-equ(["a", "b", "c"]);
+// equ(["a", "b", "c"]);
+equ([2, 4, 5]);
+// equ(["a", "b", "c", "d"]);
 // console.log(equ(["a", "b"]));
